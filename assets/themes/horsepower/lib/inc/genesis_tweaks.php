c080ddad621074566f066c3cd25f4298c15cf621
Catherine OBrien-Sandrick <catherine@madsciencedept.com>
2015-11-30 12:50:10 -0500

    slider styling

--- assets/themes/horsepower/lib/inc/genesis_tweaks.php
+++ assets/themes/horsepower/lib/inc/genesis_tweaks.php
@@ -20,7 +20,7 @@ add_action( 'msdlab_pre_header', 'genesis_do_subnav' );
 add_action('msdlab_pre_header','msdlab_header_right');
 
 
-add_action('genesis_after_header','msdlab_page_banner');
+//add_action('genesis_after_header','msdlab_page_banner');
 
 /*** NAV ***/
 /**
@@ -52,7 +52,7 @@ remove_action('genesis_footer','genesis_do_footer'); //replace the footer
 //add_action('genesis_footer','msdlab_do_social_footer');//with a msdsocial support one
 
 /*** HOMEPAGE (BACKEND SUPPORT) ***/
-add_action('after_setup_theme','msdlab_add_homepage_hero_flex_sidebars'); //creates widget areas for a hero and flexible widget area
+//add_action('after_setup_theme','msdlab_add_homepage_hero_flex_sidebars'); //creates widget areas for a hero and flexible widget area
 //add_action('after_setup_theme','msdlab_add_homepage_callout_sidebars'); //creates a widget area for a callout bar, usually between the hero and the widget area
 
 /*** SITEMAP ***/
